3adfb61bfe2a3b1486bc6a2a7f0fa207045550a5
Alessandro Ratti <alessandro.ratti@gmail.com>
2015-03-11 18:43:51 +0100

    Fix start_notebook.sh script

--- provisioning/tools/start_notebook.sh
+++ provisioning/tools/start_notebook.sh
@@ -3,10 +3,11 @@
 set -e
 umask 022
 
+. /lib/lsb/init-functions
+
 case "$1" in
 start)
 	SSHD_OPTS="--ip=0.0.0.0 --no-browser --notebook-dir=/home/vagrant/Tutorials"
-	. /lib/lsb/init-functions
 	log_daemon_msg "Starting Notebook" || true
 		if /sbin/start-stop-daemon --background --start --quiet --oknodo --pidfile /var/run/notebook.pid --exec /home/vagrant/anaconda/bin/ipython notebook -- $SSHD_OPTS; then
 		    log_end_msg 0 || true
@@ -15,13 +16,13 @@ start)
 		fi
 	;;
 stop)
-        log_daemon_msg "Stopping Notebook" || true
-        if start-stop-daemon --stop --quiet --oknodo --pidfile /var/run/notebook.pid.pid; then
-            log_end_msg 0 || true
-        else
-            log_end_msg 1 || true
-        fi
-        ;;
+    log_daemon_msg "Stopping Notebook" || true
+    if /sbin/start-stop-daemon --stop --quiet --oknodo --pidfile /var/run/notebook.pid.pid; then
+        log_end_msg 0 || true
+    else
+        log_end_msg 1 || true
+    fi
+    ;;
 esac
 
 
