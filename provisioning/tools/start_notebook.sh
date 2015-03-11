ae54b2b4afba36667cfb42e705170e2e4b1de6a7
Alessandro Ratti <alessandro.ratti@gmail.com>
2015-03-11 16:44:58 +0100

    Add Ansible playbook to provision anaconda

---
+++ provisioning/tools/start_notebook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+umask 022
+
+case "$1" in
+start)
+	SSHD_OPTS="--ip=0.0.0.0 --no-browser --notebook-dir=/home/vagrant/Tutorials"
+	. /lib/lsb/init-functions
+	log_daemon_msg "Starting Notebook" || true
+		if /sbin/start-stop-daemon --background --start --quiet --oknodo --pidfile /var/run/notebook.pid --exec /home/vagrant/anaconda/bin/ipython notebook -- $SSHD_OPTS; then
+		    log_end_msg 0 || true
+		else
+		    log_end_msg 1 || true
+		fi
+	;;
+stop)
+        log_daemon_msg "Stopping Notebook" || true
+        if start-stop-daemon --stop --quiet --oknodo --pidfile /var/run/notebook.pid.pid; then
+            log_end_msg 0 || true
+        else
+            log_end_msg 1 || true
+        fi
+        ;;
+esac
+
+
+exit 0
